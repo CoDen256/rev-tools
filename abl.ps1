d6126cf1617a11802d176f1623e36212029286db
Denys Chernyshov <den.blackshov@gmail.com>
2024-10-04 17:25:08 +0200

    add stuff

--- abl.ps1
+++ abl.ps1
@@ -1,7 +1,7 @@
 $apkfile=$args[0]
 $name = Split-Path $apkfile -leaf
 
-apktool.cmd b "$apkfile" -o "$name.b.apk"
+apktool.cmd b --use-aapt2 "$apkfile" -o "$name.b.apk"
 zipalign.exe -v -p 4 "$name.b.apk" "$name.z.apk"
 $signed = asg.ps1 "$name.z.apk"
 @("$name.b.apk", "$name.z.apk", $signed)
---
+++ merger.ps1
@@ -0,0 +1,1 @@
+python.exe merger.py
